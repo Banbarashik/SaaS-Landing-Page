d4e421b10fed8051759ce7a73936f2a550622f0b
Banbarashik <odinokyskitalec@yahoo.com>
2024-06-12 20:03:57 +0400

    add event to toggle-switch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './sass/main.scss';
+
+const toggleSwitch = document.querySelector('.toggle-switch');
+
+toggleSwitch.addEventListener('click', function (e) {
+  e.target.classList.toggle('toggle-switch--right');
+});
