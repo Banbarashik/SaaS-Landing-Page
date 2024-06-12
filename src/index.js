9fee4fbad615955368b5c98020d9635fe6117d2a
Banbarashik <odinokyskitalec@yahoo.com>
2024-06-12 22:23:16 +0400

    implement style change for toggle-with-buttons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import './sass/main.scss';
 
+const toggleWithButtons = document.querySelector('.toggle-with-buttons');
 const toggleSwitch = document.querySelector('.toggle-switch');
+
+toggleWithButtons.addEventListener('input', function (e) {
+  const toggle = e.currentTarget;
+  const [label] = e.target.labels;
+
+  toggle
+    .querySelectorAll('.toggle-button')
+    .forEach((button) => button.classList.remove('toggle-button--active'));
+
+  label.classList.add('toggle-button--active');
+});
 
 toggleSwitch.addEventListener('click', function (e) {
   e.target.classList.toggle('toggle-switch--right');
