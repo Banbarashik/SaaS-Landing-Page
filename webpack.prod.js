3418e23c59e793552d2686ccb6b39e12a3fa6de7
Banbarashik <odinokyskitalec@yahoo.com>
2024-08-08 13:16:53 +0400

    minify css on build

--- webpack.prod.js
+++ webpack.prod.js
@@ -15,14 +15,7 @@ module.exports = merge(common, {
     rules: [
       {
         test: /\.scss$/i,
-        use: [
-          { loader: MiniCssExtractPlugin.loader },
-          { loader: 'css-loader' },
-          {
-            loader: 'sass-loader',
-            options: { sassOptions: { outputStyle: 'expanded' } },
-          },
-        ],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
     ],
   },
